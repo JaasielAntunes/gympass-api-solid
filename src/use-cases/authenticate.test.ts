c2ec0fd52c0f8a28ed43899dc0110d3774bbc2b9
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-22 17:47:58 -0300

    Refatorando instâncias nos testes

--- src/use-cases/authenticate.test.ts
+++ src/use-cases/authenticate.test.ts
@@ -1,14 +1,19 @@
-import { expect, test, describe } from "vitest";
+import { expect, test, describe, beforeEach } from "vitest";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { AuthenticateUseCase } from "./authenticate";
 import { hash } from "bcryptjs";
 import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 
-describe("Caso de uso de autenticação", () => {
-  test("Deve ser possível realizar autenticação", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(usersRepository);
+let usersRepository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
 
+describe("Caso de uso de autenticação", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
+  test("Deve ser possível realizar autenticação", async () => {
     await usersRepository.create({
       name: "Carlos Alves",
       email: "carlos@example.com",
@@ -24,9 +29,6 @@ describe("Caso de uso de autenticação", () => {
   });
 
   test("Não deve ser possível realizar autenticação com email inválido", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(usersRepository);
-
     expect(() =>
       sut.execute({
         email: "carlos@example.com",
@@ -36,9 +38,6 @@ describe("Caso de uso de autenticação", () => {
   });
 
   test("Não deve ser possível realizar autenticação com senha inválida", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(usersRepository);
-
     expect(() =>
       sut.execute({
         email: "carlos@example.com",
--- src/use-cases/register.test.ts
+++ src/use-cases/register.test.ts
@@ -1,39 +1,51 @@
-import { expect, test, describe } from "vitest";
+import { expect, test, describe, beforeEach } from "vitest";
 import { RegisterUseCase } from "./register";
-// import { compare } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { UserAlreadyExistsError } from "./errors/user-alredy-exists";
+import { compare } from "bcryptjs";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: RegisterUseCase;
 
 describe("Caso de uso de cadastro", () => {
-  test("Deve ser gerado um hash de senha no momento do cadastro", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new RegisterUseCase(usersRepository);
+  });
 
-    const { user } = await registerUseCase.execute({
+  test("Deve ser possível realizar o cadastro", async () => {
+    const { user } = await sut.execute({
       name: "Carlos Alves",
       email: "carlos@example.com",
       password: "1234",
     });
 
-    // const isPasswordCorrectlyHashed = await compare("1234", user.password_hash);
-
     expect(user.id).toEqual(expect.any(String));
   });
 
-  test("Não deve ser possivel se cadastrar com o mesmo email", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+  test("Deve ser gerado um hash de senha no momento do cadastro", async () => {
+    const { user } = await sut.execute({
+      name: "Carlos Alves",
+      email: "carlos@example.com",
+      password: "1234",
+    });
 
+    const isPasswordCorrectlyHashed = await compare("1234", user.password_hash);
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  test("Não deve ser possivel se cadastrar com o mesmo email", async () => {
     const email = "carlos@example.com";
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: "Carlos Alves",
       email,
       password: "1234",
     });
 
     await expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: "Carlos Alves",
         email,
         password: "1234",
