1d820b63ad4d1e5100c9370358150f68b7933170
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-27 11:11:51 -0300

    Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,19 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(req: FastifyRequest, res: FastifyReply) {
   await req.jwtVerify();
 
-  console.log(req.user.sub);
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return res.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
+
+  return res.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
