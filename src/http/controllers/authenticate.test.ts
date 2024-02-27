110ddd6cead2e450742d7574a97339f702118ead
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-27 15:09:56 -0300

    Teste E2E do perfil

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "@/app";
 import { afterAll, beforeAll, describe, expect, test } from "vitest";
 
-describe("Autenticação (e2e)", () => {
+describe("Perfil (e2e)", () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -11,21 +11,30 @@ describe("Autenticação (e2e)", () => {
     await app.close();
   });
 
-  test("Deve ser possível realizar autenticação", async () => {
+  test("Deve ser possível obter o perfil", async () => {
     await request(app.server).post("/users").send({
       name: "Teste E2E",
       email: "testee2e@example.com",
       password: "123321",
     });
 
-    const response = await request(app.server).post("/sessions").send({
+    const authResponse = await request(app.server).post("/sessions").send({
       email: "testee2e@example.com",
       password: "123321",
     });
 
-    expect(response.statusCode).toEqual(200);
-    expect(response.body).toEqual({
-      token: expect.any(String),
-    });
+    const { token } = authResponse.body;
+
+    const profileResponse = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(profileResponse.statusCode).toEqual(200);
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: "testee2e@example.com",
+      }),
+    );
   });
 });
