7f318aa9ae83c0911093671c2b1d911f04630509
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-27 14:33:39 -0300

    Test Environment do Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,30 @@
 /* eslint-disable prettier/prettier */
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+
+// "postgresql://docker:docker@localhost:5432/apisolid?schema=public"
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Forneça uma variável de ambiente DATABASE_URL!");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schema);
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("setup");
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       teardown() {
@@ -13,5 +33,5 @@ export default <Environment>{
     };
   },
 
-  transformMode: "web",
+  transformMode: "ssr",
 };
