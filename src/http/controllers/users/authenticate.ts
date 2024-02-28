d070fcf97d37c36a190d89bf0dce8cd0a8e77a16
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-28 11:43:15 -0300

    Refresh de token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -28,9 +28,27 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
       },
     );
 
-    return res.status(200).send({
-      token,
-    });
+    const refreshToken = await res.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "5d",
+        },
+      },
+    );
+
+    return res
+      .status(200)
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .send({
+        token,
+      });
   } catch (e) {
     if (e instanceof InvalidCredentialsError) {
       return res.status(400).send({ message: e.message });
